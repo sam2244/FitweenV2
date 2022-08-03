68bccc5d4235a09b135c5672fbc5b0f075a26de6
Hamgule <hsj6831@naver.com>
2022-08-03 11:45:24 +0900

    Merged

--- lib/view/page/challenge/timer/timer.dart
+++ lib/view/page/challenge/timer/timer.dart
@@ -5,6 +5,11 @@ class TimerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Container(),
+      ),
+    );
   }
 }
